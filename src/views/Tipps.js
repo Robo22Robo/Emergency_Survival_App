d7d041144d79372f2c5a8e40e3ad93a82e1c24ec
Robert Krawczyk <robo22robo@yahoo.de>
2020-04-18 21:20:16 +0200

    tracking map

--- src/views/Tipps.js
+++ src/views/Tipps.js
@@ -1,57 +1,14 @@
-import React, { useEffect, useState } from "react";
-import { csv } from "d3-fetch";
-import { scaleLinear } from "d3-scale";
-import {
-  ComposableMap,
-  Geographies,
-  Geography,
-  Sphere,
-  Graticule,
-} from "react-simple-maps";
+import React from "react";
 
-const geoUrl =
-  "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
-
-const colorScale = scaleLinear()
-  .domain([0.29, 0.68])
-  .range(["#ffedea", "#ff5233"]);
-
-const MapChart = () => {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    csv(`/vulnerability.csv`).then((data) => {
-      setData(data);
-    });
-  }, []);
-
+export default function About() {
   return (
-    <ComposableMap
-      projectionConfig={{
-        rotate: [-10, 0, 0],
-        scale: 147,
-      }}
-    >
-      <Sphere stroke="#E4E5E6" strokeWidth={0.5} />
-      <Graticule stroke="#E4E5E6" strokeWidth={0.5} />
-      {data.length > 0 && (
-        <Geographies geography={geoUrl}>
-          {({ geographies }) =>
-            geographies.map((geo) => {
-              const d = data.find((s) => s.ISO3 === geo.properties.ISO_A3);
-              return (
-                <Geography
-                  key={geo.rsmKey}
-                  geography={geo}
-                  fill={d ? colorScale(d["2017"]) : "#F5F4F6"}
-                />
-              );
-            })
-          }
-        </Geographies>
-      )}
-    </ComposableMap>
+    <div>
+      <iframe
+        width="1024"
+        height="576"
+        src="https://app.developer.here.com/coronavirus/"
+        frameborder="0"
+      ></iframe>
+    </div>
   );
-};
-
-export default MapChart;
+}
