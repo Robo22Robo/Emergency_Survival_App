dae37355d8a4445def5999b6bfde156ed67c06da
Robert Krawczyk <robo22robo@yahoo.de>
2020-04-20 12:05:12 +0200

    Table - sorting

--- src/views/Infizierte.js
+++ src/views/Infizierte.js
@@ -1,17 +1,65 @@
 import React from "react";
+import ObjectList from "react-object-list";
 import "../styles/Infizierte.scss";
 export default class Infizierte extends React.Component {
   state = {
     loading: true,
     land: null,
+    sortKeys: [],
   };
   async componentDidMount() {
     const url = "https://api.thevirustracker.com/free-api?countryTotals=ALL";
     const response = await fetch(url);
     const data = await response.json();
-    this.setState({ land: data.countryitems[0], loading: false });
-    console.log(data.countryitems[0]);
+    const dataset = data["countryitems"][0];
+    const countryitems = Object.keys(dataset).map((id, key) => {
+      return {
+        land: dataset[id].title,
+        total: dataset[id].total_cases,
+        active: dataset[id].total_active_cases,
+        recovered: dataset[id].total_recovered,
+        unresolved: dataset[id].total_unresolved,
+        deaths: dataset[id].total_deaths,
+        new_cases_today: dataset[id].total_new_cases_today,
+        new_deaths_today: dataset[id].total_new_deaths_today,
+        serious_cases: dataset[id].total_serious_cases,
+      };
+    });
+    console.log(countryitems);
+    this.setState({
+      countryitems,
+      land: data["countryitems"][0],
+      loading: false,
+    });
   }
+  updateSorting = (sortKey) =>
+    this.setState((prevState) => {
+      let sortKeys = [...prevState.sortKeys];
+      const currentSort = sortKeys.find((sort) => sort.sortKey === sortKey);
+      let value = true;
+      if (currentSort !== undefined && currentSort.value === true) {
+        value = false;
+      }
+      sortKeys = [{ sortKey: sortKey, value }].concat(
+        sortKeys.filter((k) => k.sortKey !== sortKey)
+      );
+      const offset = (prevState.currentPage - 1) * prevState.perPage;
+      return {
+        sortKeys,
+        data: this.state.countryitems
+          .sort((a, b) => {
+            for (let i = 0; i < sortKeys.length; i++) {
+              const order = sortKeys[i].value ? 1 : -1;
+              if (a[sortKeys[i].sortKey] > b[sortKeys[i].sortKey])
+                return -1 * order;
+              if (a[sortKeys[i].sortKey] < b[sortKeys[i].sortKey])
+                return 1 * order;
+            }
+            return 0;
+          })
+          .slice(offset, offset + prevState.perPage),
+      };
+    });
   render() {
     return (
       <div>
@@ -20,77 +68,41 @@ export default class Infizierte extends React.Component {
         ) : (
           <div>
             <div>
-              <table className="Infected-Table">
-                <thead>
-                  <tr>
-                    <th>Land</th>
-                    <th>
-                      total
-                      <br />
-                      cases
-                    </th>
-                    <th>
-                      total
-                      <br />
-                      active
-                      <br />
-                      cases
-                    </th>
+              <ObjectList
+                columns={[
+                  { dataKey: "land", header: "Land", sortKey: "land" },
+                  { dataKey: "total", header: "Total", sortKey: "total" },
+                  { dataKey: "active", header: "Active", sortKey: "active" },
+                  {
+                    dataKey: "recovered",
+                    header: "Recovered",
+                    sortKey: "recovered",
+                  },
+                  { dataKey: "deaths", header: "Death", sortKey: "deaths" },
 
-                    <th>
-                      total
-                      <br />
-                      recovered
-                    </th>
-
-                    <th>
-                      total
-                      <br />
-                      deaths
-                    </th>
-                    <th>
-                      total new
-                      <br />
-                      cases
-                      <br />
-                      today
-                    </th>
-                    <th>
-                      total new
-                      <br />
-                      deaths
-                      <br />
-                      today
-                    </th>
-                    <th>
-                      total
-                      <br />
-                      serious
-                      <br />
-                      cases
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {!this.state.loading &&
-                    Object.keys(this.state.land).map((id, key) => {
-                      return (
-                        <tr key={key}>
-                          <td>{this.state.land[id].title}</td>
-                          <td>{this.state.land[id].total_cases}</td>
-                          <td>{this.state.land[id].total_active_cases}</td>
-
-                          <td>{this.state.land[id].total_recovered}</td>
-
-                          <td>{this.state.land[id].total_deaths}</td>
-                          <td>{this.state.land[id].total_new_cases_today}</td>
-                          <td>{this.state.land[id].total_new_deaths_today}</td>
-                          <td>{this.state.land[id].total_serious_cases}</td>
-                        </tr>
-                      );
-                    })}
-                </tbody>
-              </table>
+                  {
+                    dataKey: "new_cases_today",
+                    header: "new cases today",
+                    sortKey: "new_cases_today",
+                  },
+                  {
+                    dataKey: "new_deaths_today",
+                    header: "new deaths today",
+                    sortKey: "new_deaths_today",
+                  },
+                  {
+                    dataKey: "serious_cases",
+                    header: "serious cases",
+                    sortKey: "serious_cases",
+                  },
+                ]}
+                data={this.state.countryitems}
+                updateSorting={this.updateSorting}
+                meta={{
+                  totalCount: this.state.countryitems.length,
+                }}
+                favouritesEnabled={false}
+              />
             </div>
           </div>
         )}
