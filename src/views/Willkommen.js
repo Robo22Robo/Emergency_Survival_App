b5e5314c6aade99e876b46d3fc59d699482ea2d5
Rob <robo22robo@yahoo.de>
2020-04-29 10:23:52 +0200

    Willkommen

--- src/views/Willkommen.js
+++ src/views/Willkommen.js
@@ -8,12 +8,19 @@ export default function Willkommen() {
         className="supplies-container container transparent d-flex flex-column"
         style={{ backgroundColor: "white" }}
       >
-        <h2 style={{ marginTop: "0px" }}>Willkommen</h2>
-        <p>asfdsa</p>
-
-        <p>asfdsa</p>
-        <p>asfdsa</p>
-        <p>asfdsa</p>
+        <h2 style={{ marginTop: "0px", marginBottom: "5px" }}>
+          Willkommen auf meiner Webseite
+        </h2>
+        <h3 style={{ paddingBottom: "5px" }}>Hier erwarten dich:</h3>
+        <br />
+        <p>Nützliche Survival Tipps.</p>
+        <p>Tipps wie man sich von Coronavirus schützen kann</p>
+        <p>Wie man Knoten macht</p>
+        <br />
+        <p>Welche Vorräte man sich vorbereiten sollte und warum.</p>
+        <p>Die Statistik über mit Coronavirus infizierten.</p>
+        <p>Eine interaktive Karte mit Coronavirus infizierten.</p>
+        <p>Sie können mich auch gerne kontaktieren. </p>
       </div>
     </div>
   );
